661e2ad70b1f98262b3b175900fcbcde44d60e78
NinjaChucks <158800299+Chucks1234@users.noreply.github.com>
2024-11-20 11:20:18 -0600

    Add files via upload

---
+++ TTT/Cell.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cell
+{
+    public PlayerOption current = PlayerOption.NONE;
+}
+//part of player input
